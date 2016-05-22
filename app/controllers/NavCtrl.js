0a8133c98f3fa4514ff325dea2d85bd2cf2b041b
Dylan Thomas <kdylanthomas@gmail.com>
2016-05-22 16:22:08 -0500

    changed 'Log Out' to 'Register' in nav bar for guest user

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -11,5 +11,14 @@ app.controller('NavCtrl', [
 		$scope.checkPath = () => {
 			$scope.path = $location.path();
 		}
+
+		$scope.authMessage = "Log Out";
+
+		// determines if guest is using site and provides "Register" or "Log Out" message in nav
+		let currUser = authenticate.getCurrentUser();
+		if (currUser.uid === "23aa05d9-85df-47ab-86dd-a9ab0cbadc19") {
+			$scope.authMessage = "Register";
+		}
+
 	}
 ])
